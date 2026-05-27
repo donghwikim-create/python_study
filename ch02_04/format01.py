2023272d379fb37093e9b056d8f8968efef19d77
donghwikim00 <cvasq2103@gmail.com>
2026-05-27 15:51:35 +0900

    format() 함수를 활용한 다양한 예제

---
+++ ch02_04/format01.py
@@ -0,0 +1,10 @@
+string_a = "{}만원".format(5000)
+string_b = "코딩 공부해서 연봉 {}만원 받기".format(3100)
+string_c = "{}, {}, {}".format(3000, 4000, 5000)
+string_d = "{}, {}, {}".format(1, "문자열", True)
+
+# 출력
+print(string_a)
+print(string_b)
+print(string_c)
+print(string_d)
