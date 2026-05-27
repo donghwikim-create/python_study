ec429997c49efcd51c13b57afcdb2ec9e2781f3f
donghwikim00 <cvasq2103@gmail.com>
2026-05-27 16:26:15 +0900

    format() 함수 예제: 소수점 아래 자릿수 지정하기

---
+++ ch02_04/format06.py
@@ -0,0 +1,9 @@
+# 15칸 뒤에 소수점 3자리,2자리,1자리 출력하는 코드
+
+output_a="{:15.3f}".format(52.273) # 소수점 3자리
+output_b="{:15.2f}".format(52.273) # 소수점 2자리
+output_c="{:15.1f}".format(52.273) # 소수점 1자리
+
+print(output_a)
+print(output_b)
+print(output_c)
