a08520b651964f9c0a274e63110cfb62615136ef
donghwikim00 <cvasq2103@gmail.com>
2026-05-27 14:54:10 +0900

    int() 함수 활용 예제

---
+++ ch02_3/int_convert.py
@@ -0,0 +1,12 @@
+# 값 2개를 입력받고
+# 입력받은 값끼리 더해서 출력하는 코드 한줄
+# 입력받은 값을 정수형으로 변환하고 더한 코드 한줄을 만드시오.
+
+string_a = input("첫번쨰 값을 입력하시오:")
+string_b = input("두번쨰 값을 입력하시오:")
+
+int_a = int(string_a)
+int_b = int(string_b)
+
+print("두 문자열의 합:", string_a + string_b)
+print("두 문자열을 정수 자료형으로 바꾼 값의 합:", int_a + int_b)
