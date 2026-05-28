fc60a6baf95133a4d332816ccd90552a582b83c2
donghwikim00 <cvasq2103@gmail.com>
2026-05-28 15:34:47 +0900

    not 연산자 활용 예제

---
+++ ch03_01/boolean.py
@@ -0,0 +1,9 @@
+# x라는 변수에 10이라는 값의 단항 연산자를 할당하고
+# under_20 이라는 변수에 x < 20 이라는 이항 연산자를 할당하고
+# under_20을 그냥 출력
+# under_20을 not 연산자를 활용해서 출력
+
+x = 10
+under_20 = x < 20
+print("under_20:", under_20) # True
+print("not under_20:", not under_20) # False
