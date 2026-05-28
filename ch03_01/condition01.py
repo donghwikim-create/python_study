ddb9de2c5b67b2dfa9676cdaff0a0a2930e50d18
donghwikim00 <cvasq2103@gmail.com>
2026-05-28 16:33:32 +0900

    끝자리로 홀수와 짝수 구분

---
+++ ch03_01/condition01.py
@@ -0,0 +1,12 @@
+# 끝자리로 홀수와 짝수 구분
+str_num = input("숫자를 입력해주세요:")
+
+last_num = str_num[-1]
+
+int_num = int(last_num)
+
+if int_num == 0  or int_num == 2 or int_num == 4 or int_num == 6 or int_num == 8:
+    print("이 숫자는 짝수입니다.")
+
+ # 처음에는 그냥 정수 타입으로 입력받았지만 마지막 숫자를 알아내려면 정수 타입이 아닌 
+ # 문자열로 입력받아 마지막에 정수 타입으로 변경해야함  
