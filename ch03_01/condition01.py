cd880f38bd928c472217008baa604c1c6eb5e9db
donghwikim00 <cvasq2103@gmail.com>
2026-05-28 16:42:19 +0900

    짝수 구분, 홀수 구분 줄바꿈 추가, in 연산자를 활용한 짝수 홀수 구분 프로그램 작성

--- ch03_01/condition01.py
+++ ch03_01/condition01.py
@@ -6,11 +6,19 @@ last_num = str_num[-1]
 int_num = int(last_num)
 
 # 짝수 구분
-if int_num == 0  or int_num == 2 or int_num == 4 or int_num == 6 or int_num == 8:
+if int_num == 0\
+    or int_num == 2\
+          or int_num == 4\
+              or int_num == 6\
+                  or int_num == 8:
     print("이 숫자는 짝수입니다.")
 
 # 홀수 구분
-if int_num ==1 or int_num == 3 or int_num == 5 or int_num == 7 or int_num == 9:
+if int_num ==1\
+      or int_num == 3\
+          or int_num == 5\
+              or int_num == 7\
+                  or int_num == 9:
     print("이 숫자는 홀수입니다.")
 
 
