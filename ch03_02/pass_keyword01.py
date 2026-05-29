c3237f3bf6597c8490309748cd7080adb3539e89
donghwikim00 <cvasq2103@gmail.com>
2026-05-29 16:18:46 +0900

    if 조건문에 아직 미구현일 경우 파이썬에서는 pass라는 단어를 넣어준다.

---
+++ ch03_02/pass_keyword01.py
@@ -0,0 +1,15 @@
+# 파이썬은 if 조건문에 무조건 들여쓰기 4칸을 넣고 코드를 작성해야
+# 하므로 IndentationError가 발생
+# 그래서 이런 경우를 해결하기 위해 pass 키워드 사용
+
+# 입력을 받습니다.
+number = input("정수 입력>")
+number = int(number)
+
+# 조건문 사용
+if(number>0):
+    # 양수일 때: 아직 미구현 상태입니다
+    pass
+else:
+    # 음수일 떄: 아직 미구현 상태입니다
+    pass
