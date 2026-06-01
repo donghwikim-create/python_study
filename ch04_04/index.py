ae0afddbbb8cce50b63aff8bf4ab0f3dc053c90c
donghwikim00 <cvasq2103@gmail.com>
2026-06-01 18:45:29 +0900

    mim(), max(), sum() 함수

---
+++ ch04_04/index.py
@@ -0,0 +1,10 @@
+number = [103, 52, 273, 32, 77]
+
+print(min(number))
+print(max(number))
+print(sum(number))
+
+# 리스트 따로 저장하지 않고 숫자를 나열해도 최솟값, 최대값을 구할 수 있음
+
+print(min(103, 52, 273)) 
+print(max(103, 52, 273))
