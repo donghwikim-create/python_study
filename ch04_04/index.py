19678e1bf1c6c65920f0ca04e3ce24e0d51e490d
donghwikim00 <cvasq2103@gmail.com>
2026-06-02 16:58:29 +0900

    enumerate() 함수와 리스트

--- ch04_04/index.py
+++ ch04_04/index.py
@@ -11,28 +11,42 @@
 
 # ---
 
-temp = reversed([1, 2, 3, 4, 5, 6])
+# temp = reversed([1, 2, 3, 4, 5, 6])
 
-for i in temp:
-    print("첫 번쨰 반복문: {}".format(i))
+# for i in temp:
+#     print("첫 번쨰 반복문: {}".format(i))
 
-for i in temp:
-    print("두 번쨰 반복문: {}".format(i))
+# for i in temp:
+#     print("두 번쨰 반복문: {}".format(i))
 
-# 코드를 실행하면 첫 번째 반복문은 출력이 되지만
-# 두번째 반복문은 출력되지않음
-# 그 이유는 reversed() 함수는 제너레이터 이기 때문이다.
+# # 코드를 실행하면 첫 번째 반복문은 출력이 되지만
+# # 두번째 반복문은 출력되지않음
+# # 그 이유는 reversed() 함수는 제너레이터 이기 때문이다.
+
+# # ---
+
+# # 확장 슬라이싱
+
+# numbers = [1, 2, 3, 4, 5]
+
+# print(numbers)
+
+# print(numbers[::-1])
+
+# # 비파괴적 코드 이므로 원본 numbers에는 영향이 없다.
+
+# print("안녕하세요"[::1])
 
 # ---
 
-# 확장 슬라이싱
+example_list = ["요소A", "요소B", "요소C"]
+i = 0
 
-numbers = [1, 2, 3, 4, 5]
+for item in example_list:
+    print("{}번째 요소는 {}입니다.".format(i, example_list))
+    i+=1
 
-print(numbers)
+print()
 
-print(numbers[::-1])
-
-# 비파괴적 코드 이므로 원본 numbers에는 영향이 없다.
-
-print("안녕하세요"[::1])
+for i in range(len(example_list)):
+    print("{}번째 요소는 {}입니다.".format(i, example_list))
