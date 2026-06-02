f0cac33bf8d3613c35d12ded7b389e5f7d049573
donghwikim00 <cvasq2103@gmail.com>
2026-06-02 16:42:07 +0900

    제네레이터와 확장 슬라이싱

--- ch04_04/index.py
+++ ch04_04/index.py
@@ -1,10 +1,38 @@
-number = [103, 52, 273, 32, 77]
+# number = [103, 52, 273, 32, 77]
 
-print(min(number))
-print(max(number))
-print(sum(number))
+# print(min(number))
+# print(max(number))
+# print(sum(number))
 
-# 리스트 따로 저장하지 않고 숫자를 나열해도 최솟값, 최대값을 구할 수 있음
+# # 리스트 따로 저장하지 않고 숫자를 나열해도 최솟값, 최대값을 구할 수 있음
 
-print(min(103, 52, 273)) 
-print(max(103, 52, 273))
+# print(min(103, 52, 273)) 
+# print(max(103, 52, 273))
+
+# ---
+
+temp = reversed([1, 2, 3, 4, 5, 6])
+
+for i in temp:
+    print("첫 번쨰 반복문: {}".format(i))
+
+for i in temp:
+    print("두 번쨰 반복문: {}".format(i))
+
+# 코드를 실행하면 첫 번째 반복문은 출력이 되지만
+# 두번째 반복문은 출력되지않음
+# 그 이유는 reversed() 함수는 제너레이터 이기 때문이다.
+
+# ---
+
+# 확장 슬라이싱
+
+numbers = [1, 2, 3, 4, 5]
+
+print(numbers)
+
+print(numbers[::-1])
+
+# 비파괴적 코드 이므로 원본 numbers에는 영향이 없다.
+
+print("안녕하세요"[::1])
