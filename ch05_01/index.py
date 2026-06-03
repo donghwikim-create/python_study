1b4d740ff2a31306fa58c6dbdb0f798b6bd1adce
donghwikim00 <cvasq2103@gmail.com>
2026-06-03 18:30:46 +0900

    가변 매개변수 뒤에는 일반 매개변수가 올 수 없다

--- ch05_01/index.py
+++ ch05_01/index.py
@@ -1,8 +1,13 @@
-def mul(*values):
-    output = 1
-    for value in values:
-        output *= value
+# def mul(*values):
+#     output = 1
+#     for value in values:
+#         output *= value
     
-    return output
+#     return output
 
-print(mul(5, 7, 9, 10))
+# print(mul(5, 7, 9, 10))
+
+def function(*value, valueA, valueB): # 가변 매개변수 뒤에는 일반 매개변수가 올수가 없다. 
+    pass                              # 왜냐하면 어디까지가 가변 매개변수 인지 모르기 때문에
+
+function(1, 2, 3, 4, 5)
