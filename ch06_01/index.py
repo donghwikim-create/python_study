422026a0fb7fdd31f25b51c15b25274013e98082
donghwikim00 <cvasq2103@gmail.com>
2026-06-11 23:20:05 +0900

    확인문제

---
+++ ch06_01/index.py
@@ -0,0 +1,39 @@
+# numbers = [52, 273, 32, 103, 90, 10, 275]
+
+# print("# (1) 요소 내부에 있는 값 찾기")
+# print("- {}는 {} 위치에 있습니다.".format(52, numbers.index(52))) # format 함수를 쓸떄는 .(점)으로 연결한다.
+# print()
+
+# print("# (2) 요소 내부에 없는 값 찾기")
+# number = 10000
+# try:
+#     print("- {}는 {}위치에 있습니다.".format(number, numbers.index(number)))
+# except:
+#     print("리스트 내부에 없는 값입니다.")
+# print()
+
+# print("--- 정상적으로 종료되었습니다. ---")
+
+# print("# (2) 요소 내부에 없는 값 찾기")
+# number = 10000
+# if number in numbers:
+#     print("- {}는 {}위치에 있습니다.".format(number, numbers.index(number)))
+# else:
+#     print("리스트 내부에 없는 값입니다.")
+# print()
+
+# print("--- 정상적으로 종료되었습니다. ---")
+
+# ----
+
+# 구문오류 : 코드 자체가 성립이 안되는 것 실행 자체가 안됨
+# 예외 : 코드자체는 성립은 하지만 실행시 error가 발생함.
+
+# output = 10 + "개"
+# print(output)
+
+# int("안녕하세요")
+
+# cursor.close)
+
+[1, 2, 3, 4, 5][10] # 앞에 리스트에서 10번째 방 번호를 가져오겠다.
